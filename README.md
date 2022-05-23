ff95e1986c15238232e0f780469a556fb504e7d6
SULTAN-MURAT <98175937+SULTAN-MURAT@users.noreply.github.com>
2022-05-23 19:13:35 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# new-npm-package-react
+# button-uix
+
+> test buttons ui
+
+[![NPM](https://img.shields.io/npm/v/button-uix.svg)](https://www.npmjs.com/package/button-uix) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save button-uix
+```
+
+## Usage
+
+```jsx
+import React, { Component } from 'react'
+
+import MyComponent from 'button-uix'
+import 'button-uix/dist/index.css'
+
+class Example extends Component {
+  render() {
+    return <MyComponent />
+  }
+}
+```
+
+## License
+
+MIT © [Murat-Han](https://github.com/Murat-Han)
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './styles.module.css'
+
+export const ExampleComponent = ({ text }) => {
+  return <div className={styles.test}>Example Component: {text}</div>
+}
+
+export const Button = ({ type, text }) => {
+  switch (type.toLowerCase()) {
+    case 'primary':
+      return <button className={styles.primary}>{text}</button>
+      break
+    case 'default':
+      return <button className={styles.default}>{text}</button>
+      break
+    case 'dashed':
+      return <button className={styles.dashed}>{text}</button>
+      break
+    case 'text':
+      return <button className={styles.text}>{text}</button>
+      break
+    case 'link':
+      return <button className={styles.link}>{text}</button>
+      break
+    default:
+      return <button className={styles.default}>{text}</button>
+  }
+}
+
+Button.defaultProps = {
+  type: "default",
+  text: "Click"
+}
+Button.prototypes = {
+  type: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
+}
